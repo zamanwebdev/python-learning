aaa20c473db69888a5f80608acee8da743f26df8
Syed Zaman <zamanwebdev@gmail.com>
2026-02-01 21:14:38 +0600

    Day 02 Practice Done.

---
+++ Day02.py
@@ -0,0 +1,36 @@
+name = "Zaman"
+age = 25
+country = "Bangladesh"
+print(name)
+print(age)
+print(country)
+# Example
+price = 99.99
+is_student = True
+print(price)
+print(is_student)
+# Real-Life Example
+product = "laptop"
+price = 50000
+in_stock = True
+print("Product: ", product)
+print("Price: ", price)
+print("Available:", in_stock)
+print(type(product))
+print(type(price))
+print(type(in_stock))
+# DAY 2 TASK
+name = "Syed Zaman"
+age = 31
+skill = "Python Beginner"
+future_goal = "AI Developer"
+print(name)
+print(age)
+print(skill)
+print(future_goal)
+# BONUS Practice
+x = 10
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
